61daf0c1e6cb6ab717e568eb96925e84fe809ad4
iamarshiya <202301070209@mitaoe.ac.in>
2025-12-02 18:40:59 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,4 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(<App />);
-} else {
-  console.error("Root element not found");
-}
+createRoot(document.getElementById("root")).render(<App />);
